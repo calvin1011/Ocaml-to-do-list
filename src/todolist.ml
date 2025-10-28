9f206ec235f96b3018ae299cdc3adc0a7847167d
calvin1011 <calvinssendawula@gmail.com>
2025-10-27 23:29:13 -0600

    feat: Implement core to-do list structure and add_task function

---
+++ src/todolist.ml
@@ -0,0 +1,44 @@
+(* First, we define the concrete types that were abstract in the .mli file. *)
+type task = {
+  id: int;
+  description: string;
+  completed: bool;
+}
+
+type todo_list = task list
+
+(* Getter functions to access task fields *)
+let id task = task.id
+let description task = task.description
+let completed task = task.completed
+
+(* Function implementations *)
+let create () =
+  [] (* An empty list is our empty to-do list *)
+
+let get_all_tasks list =
+  list (* Just return the list itself *)
+
+let add_task description list =
+    let next_id =
+        let find_max_id max_id task = max task.id max_id in
+        match list with 
+        | [] -> 1
+        | _ -> (List.fold_left find_max_id 0 list) + 1
+    in
+
+    (* create new task record *)
+    let new_task = {
+        id = next_id;
+        description = description;
+        completed = false;
+    } in
+
+    (* return a new list with the new task appended to the end *)
+    list @ [new_task]
+    
+let complete_task id list =
+  failwith "Unimplemented: complete_task"
+
+let remove_task id list =
+  failwith "Unimplemented: remove_task"
